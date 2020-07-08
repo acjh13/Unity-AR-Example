439cd857b139f3894bd1210ade0a887d79f2d947
SUFFERING\ACJH <acjh13@gmail.com>
2020-07-08 10:21:20 +0800

    pushed project files

---
+++ Assets/Scripts/ImgTracker.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARTrackedImageManager))]
+public class ImgTracker : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("The camera to set on the world space UI canvas far each instantiated image info.")]
+    Camera m_WorldSpaceCanvasCamera;
+
+    public Camera worldSpaceCanvasCamera
+    {
+        get { return m_WorldSpaceCanvasCamera; }
+        set { m_WorldSpaceCanvasCamera = value; }
+    }
+
+
+    [SerializeField]
+    [Tooltip("If an image is detected but no source texture can be found, this texture will be used instead.")]
+    Texture2D m_DefaultTexture;
+
+    public Texture2D defaultTexture
+    {
+        get { return m_DefaultTexture; }
+        set { m_DefaultTexture = value; }
+    }
+
+    ARTrackedImageManager m_TrackedImageMgr;
+
+    void Awake()
+    {
+        m_TrackedImageMgr = GetComponent<ARTrackedImageManager>();
+    }
+
+    void OnEnable()
+    {
+        m_TrackedImageMgr.trackedImagesChanged += OnTrackedImagesChanged;
+    }
+
+    void OnDisable()
+    {
+        m_TrackedImageMgr.trackedImagesChanged -= OnTrackedImagesChanged;
+    }
+
+    void UpdateInfo(ARTrackedImage trackedImage)
+    {
+        // Set canvas camera
+        var canvas = trackedImage.GetComponentInChildren<Canvas>();
+        canvas.worldCamera = worldSpaceCanvasCamera;
+
+        // Update info about the tracked image
+        var text = canvas.GetComponentInChildren<Text>();
+        text.text = string.Format(
+            "{0}\ntrackingState: {1}\nGUID: {2}\nReference size: {3}\nDetected size: {4} cm",
+            trackedImage.referenceImage,
+            trackedImage.trackingState,
+            trackedImage.referenceImage.guid,
+            trackedImage.referenceImage.size * 100f,
+            trackedImage.size * 100f);
+
+        var planeParentGO = trackedImage.transform.GetChild(0).gameObject;
+        var planeGO = planeParentGO.transform.GetChild(0).gameObject;
+
+        // Disable the visual plane if it is not being tracked
+        if (trackedImage.trackingState != TrackingState.None)
+        {
+            planeGO.SetActive(true);
+
+            // The image extents is only valid when the image is being tracked
+            trackedImage.transform.localScale = new Vector3(trackedImage.size.x, 1f, trackedImage.size.y);
+
+            // Set the texture
+            var material = planeGO.GetComponentInChildren<MeshRenderer>().material;
+            material.mainTexture = (trackedImage.referenceImage.texture == null) ?
+                                    defaultTexture : trackedImage.referenceImage.texture;
+        }
+        else
+        {
+            planeGO.SetActive(false);
+        }
+    }
+
+    void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        foreach (var trackedImage in eventArgs.added)
+        {
+            // Give the initial image a reasonable default scale
+            trackedImage.transform.localScale = new Vector3(0.01f, 1f, 0.01f);
+            UpdateInfo(trackedImage);
+        }
+
+        foreach (var trackedImage in eventArgs.updated)
+        {
+            UpdateInfo(trackedImage);
+        }
+    }
+}
---
+++ Assets/Scripts/SimpleRaycast.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class SimpleRaycast : MonoBehaviour
+{
+    public ARRaycastManager raycastMgr;
+    public GameObject objToPlacePrefab;
+    public Camera raycastCam;
+
+    private GameObject objInstance;
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (raycastMgr == null)
+            return;
+
+        if (raycastCam == null)
+            return;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = raycastCam.ScreenPointToRay(Input.mousePosition);
+
+            if (raycastMgr.Raycast(ray, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes))
+            {
+                Pose pose = hits[0].pose;
+                if (objInstance == null)
+                {
+                    objInstance = Instantiate<GameObject>(objToPlacePrefab, pose.position, pose.rotation);
+                }
+                else
+                {
+                    objInstance.transform.SetPositionAndRotation(pose.position, pose.rotation);
+                }
+            }
+        }
+    }
+}
